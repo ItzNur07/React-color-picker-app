bbd2d06d319fb1a8d6b919f27887f240c5db1fd5
nuruddin-99 <nuruddinalways99@gmail.com>
2021-09-30 22:26:35 +0600

    project done

---
+++ src/colors.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+export class Colors extends React.Component {
+
+	static propTypes = {
+		colorNames : PropTypes.array.isRequired
+	}
+
+	render() {
+		return (
+			<div className="row">
+				{this.props.colorNames.map(color => (
+					<div className="col" key={color} style={{backgroundColor:color}} >
+						{color}
+					</div>
+				))}
+			</div>
+		)
+	}
+}
+
+export default Colors
